bf5cc19b6dbe247d32a507570cfb87a64fd540be
Brian Musuku <brayanmusuku@gmail.com>
2024-04-14 18:26:46 +0300

    feat: new setup

--- src/components/Overlay.svelte
+++ src/components/Overlay.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import { storage } from "../storage";
-    import Options from "./Options.svelte";
+  import { onMount } from "svelte";
+  import { storage } from "../storage";
+  import Options from "./Options.svelte";
+  import { Input } from "$lib/components/ui/input";
 
-    let count = 0;
+  import "../tailwind.css";
 
-    onMount(() => {
-        storage.get().then((storage) => (count = storage.count));
-    });
+  let count = 0;
+
+  onMount(() => {
+    storage.get().then((storage) => (count = storage.count));
+  });
 </script>
 
 <div class="fixed w-80 top-4 left-4 bg-white border border-indigo-600 p-4">
-    <Options {count} />
+  <Options {count} />
+
+  <Input />
 </div>
